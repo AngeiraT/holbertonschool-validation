a15813ffcce21107fa323604a877b14cb20af31d
AngeiraT <thaispur04@icloud.com>
2023-01-09 21:15:58 -0400

    update setup file

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt install ./hugo.deb
-rm hugo.deb
+apt-get update && apt-get install -y make build-essential wget && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb
 make build
