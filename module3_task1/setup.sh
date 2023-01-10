6e3e15985103a397f506c4d8a5e495aa8af63de2
AngeiraT <thaispur04@icloud.com>
2023-01-09 21:48:31 -0400

    edit set up

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Update and install 'Hugo' and 'Make'
-apt-get update && apt-get install -y hugo make
-# Download lastest release of 'Hugo'
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-# Install the latest version of 'Hugo'
-apt install ./hugo.deb
-rm hugo.deb
-# Build the website
+apt-get update && apt-get install -y make build-essential wget && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb
 make build
