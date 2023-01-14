246a8190d5a8bebad1244424a80de18445982043
AngeiraT <thaispur04@icloud.com>
2023-01-13 21:42:01 -0400

    edited set up

--- module3_task3/setup.sh
+++ module3_task3/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update && apt-get install -y hugo make
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-sudo apt install ./hugo.deb
+apt-get install curl -y
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+apt-get install ./install_hugo.deb
 npm install -g markdownlint-cli
-rm hugo.deb
+rm ./install_hugo.deb
